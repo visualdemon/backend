39091238a9fd8ad66dcfca05478d7b2c5eacbef5
Wilber Jurado <wilberamp@gmail.com>
2022-04-26 17:37:04 -0500

    Update template.php
    Cambio lenguaje

--- Views/template.php
+++ Views/template.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <
     <title>Backend Admin</title>
 </head>
 <body>
 
 <h1>Backend admin</h1>
+
     <?php
 
     include "Modules/header.php";
