1165d10af5acf321f56cde64eb9589897ffb688f
Wilber Jurado <wilberamp@gmail.com>
2022-04-26 17:34:50 -0500

    creado al admin en plantilla html

--- Views/template.php
+++ Views/template.php
@@ -4,9 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <
     <title>Backend Admin</title>
 </head>
 <body>
+
+<h1>Backend admin</h1>
     <?php
 
     include "Modules/header.php";
