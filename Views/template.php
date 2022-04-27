6e45ed662d355cae278f3aaeb87bd144e843efb2
Wilber Jurado <wilberamp@gmail.com>
2022-04-27 17:57:36 -0500

    Anexado parte de plantilla admin template

--- Views/template.php
+++ Views/template.php
@@ -5,27 +5,49 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Backend Admin</title>
+
+    <link rel="apple-touch-icon" href="https://i.imgur.com/QRAUqs9.png">
+    <link rel="shortcut icon" href="https://i.imgur.com/QRAUqs9.png">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/normalize.css@8.0.0/normalize.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/font-awesome@4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/lykmapipo/themify-icons@0.1.2/css/themify-icons.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/pixeden-stroke-7-icon@1.2.3/pe-icon-7-stroke/dist/pe-icon-7-stroke.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.2.0/css/flag-icon.min.css">
+    <link rel="stylesheet" href="Views/assets/css/cs-skin-elastic.css">
+    <link rel="stylesheet" href="Views/assets/css/style.css">
+    <!-- <script type="text/javascript" src="https://cdn.jsdelivr.net/html5shiv/3.7.3/html5shiv.min.js"></script> -->
+    <link href="https://cdn.jsdelivr.net/npm/chartist@0.11.0/dist/chartist.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/jqvmap@1.5.1/dist/jqvmap.min.css" rel="stylesheet">
+
+    <link href="https://cdn.jsdelivr.net/npm/weathericons@2.1.0/css/weather-icons.css" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/fullcalendar@3.9.0/dist/fullcalendar.min.css" rel="stylesheet" />
 </head>
 <body>
 
-<h1>Backend admin</h1>
-
     <?php
 
-        include "Modules/header.php";
-        include "Modules/login.php";
         include "Modules/menu.php";
-        include "Modules/footer.php";
 
-        #rutas
+        echo '<div id="right-panel" class="right-panel">';
 
-        if  (isset($_GET ["ruta"])) {
-        
-            if ($_GET["ruta"] == "inicio" || $_GET["ruta"] == "perfil" || $_GET["ruta"] == "productos") {
-                include "Modules/" . $_GET["ruta"] . ".php";
-                }
+            include "Modules/header.php";
             
-        }
+            #rutas
+
+            if  (isset($_GET ["ruta"])) {
+            
+                if ($_GET["ruta"] == "inicio" || $_GET["ruta"] == "perfil" || $_GET["ruta"] == "productos") {
+                    include "Modules/" . $_GET["ruta"] . ".php";
+                    }
+                
+            }
+            
+            include "Modules/footer.php";
+
+        echo '</div>';
+
 
     ?>
     
